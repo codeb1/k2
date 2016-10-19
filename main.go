9b9fa5695febb0d9ffda8e30f0f79c0ccb915c9c
Code B <jp.trvl.tech@gmail.com>
2016-10-19 13:52:08 +0900

    Threading

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import "log"
+import "os"
+import "io"
+import "sync"
 
-func main(){
-	fmt.Printf("hello jacob \n")
+var wg = new(sync.WaitGroup)
+func worker(s int){
+//	setLog("output%s.log",s)
+     log.Printf("in worker %s\n",s)
+    wg.Done()
 }
-
+func setLog(logpath string){
+	file, err := os.Create(logpath)
+	if err	!= nil {
+		panic(err)
+	}
+	log.SetOutput(io.Writer(file))
+}
+func main(){
+	setLog("output.log")
+	log.Printf("hello jacob \n")
+	for  i:=0; i < 10;  i ++ {
+		wg.Add(1)
+		go worker(i)
+	}
+	wg.Wait()
+}

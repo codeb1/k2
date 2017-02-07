dbee2204551065a95e449bf467ffcd0d4819d7ec
Jacob Philip <jacob@c02q11l6g8wn.hgst.com>
2017-02-07 14:21:11 +0530

    Adding consumer

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ import "sync"
 
 var wg = new(sync.WaitGroup)
 
-func worker(s int) {
+func worker(s int, c chan int) {
 	log.Printf("in worker %d\n", s)
+	c <- s
 	wg.Done()
+}
+
+func reader(c chan int) {
+	log.Printf("In reader\n")
+	for true {
+		s := <-c
+		log.Printf("Read %d\n", s)
+	}
 }
 func setLog(logpath string) {
 	file, err := os.Create(logpath)
@@ -20,9 +29,13 @@ func setLog(logpath string) {
 }
 func main() {
 	setLog("output.log")
+	c := make(chan int)
+	go reader(c)
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go worker(i)
+		go worker(i, c)
+	}
+	for i := 0; i < 10; i++ {
 	}
 	wg.Wait()
 }
